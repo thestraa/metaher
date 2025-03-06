27ba3733b86e2e892700c29b5a236f56562602e0
thestraa <thestraax@gmail.com>
2025-03-06 15:04:16 +0100

    updating db

--- db.js
+++ db.js
@@ -1,35 +1,11 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
-const config = {
-  host: "survivor_mysql",
-  user: "root",
-  password: "root",
-  database: "survivor_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-};
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-function connectWithRetry(retries = 5, delay = 5000) {
-  const pool = mysql.createPool(config);
-
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error("Greška pri povezivanju sa bazom:", err);
-      if (retries > 0) {
-        console.log(`Pokušavam ponovo (${retries} preostalih)...`);
-        setTimeout(() => connectWithRetry(retries - 1, delay), delay);
-      } else {
-        console.error("Nije moguće povezati se sa bazom.");
-      }
-    } else {
-      console.log("Uspešno povezan sa MySQL bazom!");
-      connection.release();
-    }
-  });
-
-  return pool;
-}
-
-const db = connectWithRetry();
-module.exports = db;
+connection.connect((err) => {
+  if (err) {
+    console.error('Greška pri povezivanju sa bazom: ' + err.stack);
+    return;
+  }
+  console.log('Povezan sa bazom kao ID ' + connection.threadId);
+});
