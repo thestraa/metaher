3f718a2b62ec909ad79478d961bdb02eb9606fb3
thestraa <thestraax@gmail.com>
2025-03-27 17:53:55 +0100

    fs added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const uploadSitemapToNetlify = require('./uploadSitemapToNetlify'); 
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000; 
 const API_URL = process.env.API_URL || 'http://localhost:3000/api/takmicari';
@@ -223,7 +224,7 @@ app.get('/generate-sitemap', async (req, res) => {
 
     // Pozovi funkciju da upload-uješ sitemap na Netlify
     await uploadSitemapToNetlify();
-    
+
     res.status(200).send('Sitemap je uspešno generisan, sačuvan i upload-ovan na Netlify!');
   } catch (err) {
     console.error('Greška pri generisanju sitemap-a:', err);
