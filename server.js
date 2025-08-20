730468a79cedb046c7c36d55e4b70eaae0b630d2
thestraa <thestraax@gmail.com>
2025-08-20 15:59:47 +0200

    db update4

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 const cors = require("cors");
-require("dotenv").config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
